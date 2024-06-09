f7010db716929c834a7a0d381cf4c81af3796b10
LuLMJ <142363211+LuLMJ@users.noreply.github.com>
2024-06-09 11:48:12 -0300

    Delete INLAR directory

--- INLAR/inlar-back-main/prisma/migrations/20240528143406_fixed_typo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `LOUGRADOURO` on the `empresa` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `empresa` DROP COLUMN `LOUGRADOURO`,
-    ADD COLUMN `LOGRADOURO` VARCHAR(255) NULL;
