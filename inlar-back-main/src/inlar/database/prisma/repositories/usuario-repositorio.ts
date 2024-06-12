26774eb7cad601a3c9f44032316902c46efe5fde
David <david44mg@gmail.com>
2024-06-11 21:07:48 -0300

    feat/authenticate user

--- inlar-back-main/src/inlar/database/prisma/repositories/usuario-repositorio.ts
+++ inlar-back-main/src/inlar/database/prisma/repositories/usuario-repositorio.ts
@@ -31,6 +31,20 @@ export class UsuarioRepositorio {
 
     return null;
   }
+  async findByEmailAndSenha(email: string, senha: string): Promise<Usuario | null> {
+    const prismaUsuario = await this.prisma.usuario.findMany({
+      where: {
+        SENHA: senha,
+        EMAIL: email,
+      },
+    });
+
+    if (prismaUsuario ) {
+      return UsuarioMapper.fromDatabase(prismaUsuario[0]);
+    }
+
+    return null;
+  }
 
   async update(idUsuario: number, usuario: Usuario): Promise<Usuario | null> {
     const data = UsuarioMapper.toDatabase(usuario);
