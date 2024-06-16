8b3d8ae289aa4c91f2d87d26bab9cf57c1ed51b2
LuLMJ <142363211+LuLMJ@users.noreply.github.com>
2024-06-16 13:16:43 -0300

    Add files via upload

---
+++ inlar-front-main/src/app/models/interfaces/doador/responses/GetDoadorResponse.ts
@@ -0,0 +1,18 @@
+export interface GetDoadorResponse {
+  id: string;
+  nome: string;
+  tipoPessoa: string;
+  cpf?: string;
+  cnpj?: string;
+  contato1?: string;
+  contato2?: string;
+  cep?: string;
+  logradouro?: string;
+  numero?: string;
+  complemento?: string;
+  bairro?: string;
+  cidade?: string;
+  siglaEstado?: string;
+  observacoes?: string;
+  ativo: boolean;
+}
---
+++ inlar-front-main/src/app/models/interfaces/products/request/CreateProductRequest.ts
@@ -0,0 +1,8 @@
+export interface CreateProductRequest {
+  name: string;
+  price: string;
+  description: string;
+  category_id: string;
+  doador_id: string;
+  amount: number;
+}
