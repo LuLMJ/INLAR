5a46005a4c3320f0939afa7aaab499a676612175
David R Lima <david@deividy.dev>
2024-12-07 23:56:00 -0300

    fix build

--- inlar-back-main/src/http/controllers/beneficiarios/get-all-beneficiarios-controller.ts
+++ inlar-back-main/src/http/controllers/beneficiarios/get-all-beneficiarios-controller.ts
@@ -1,22 +1,15 @@
-import {
-    Controller,
-    HttpCode,
-    Get,
-    Query,
-  } from '@nestjs/common';
-import { GetAllBeneficiarios } from 'src/inlar/actions/beneficiario/get-all-beneficiarios';
-  
-  @Controller('/beneficiario-all')
-  export class GetAllBeneficiariosController {
-    constructor(private getAllBeneficiarios: GetAllBeneficiarios) {}
-  
-    @Get()
-    @HttpCode(200)
-    async handle(
-    ) {
-      const doador = await this.getAllBeneficiarios.execute();
-  
-      return doador
-    }
+import { Controller, HttpCode, Get, Query } from '@nestjs/common'
+import { GetAllBeneficiarios } from 'src/inlar/actions/beneficiario/get-all-beneficiarios'
+
+@Controller('/beneficiario-all')
+export class GetAllBeneficiariosController {
+  constructor(private getAllBeneficiarios: GetAllBeneficiarios) {}
+
+  @Get()
+  @HttpCode(200)
+  async handle() {
+    const doador = await this.getAllBeneficiarios.execute()
+
+    return doador
   }
-  
+}
